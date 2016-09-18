e0d81f63235fb6b713034fd800bf2275fc82c1f5
Dmitry_Nefedov <dmitry.nefedov@tul.cz>
2016-09-18 22:45:08 +0200

    add prof, smeny, vzdel, UP, obor

--- WorkListCR/DataModel/VolneMistaBLL.cs
+++ WorkListCR/DataModel/VolneMistaBLL.cs
@@ -6,7 +6,114 @@ using System.Threading.Tasks;
 
 namespace DataModel
 {
-    public class VolneMistaBLL
+    public static class VolneMistaBLL
     {
+        public static int saveVzdelani(string kod, string name)
+        {
+            dataClassWorkDataContext context = new dataClassWorkDataContext();
+            var vzdelan = context.vzdelans.Where(x => x.kodUP == kod).FirstOrDefault();
+            if(vzdelan!=null)
+            {
+                return ((DataModel.vzdelan)vzdelan).id;
+            }
+            else
+            {
+                DataModel.vzdelan newVzdelan = new DataModel.vzdelan()
+                {
+                    kodUP=kod,
+                    name=name
+                };
+                context.vzdelans.InsertOnSubmit(newVzdelan);
+                context.SubmitChanges();
+                return newVzdelan.id;
+            }
+        }
+
+        public static int saveSmeny(string kod, string name)
+        {
+            dataClassWorkDataContext context = new dataClassWorkDataContext();
+            var smeny = context.smenies.Where(x => x.kodUP == kod).FirstOrDefault();
+            if (smeny != null)
+            {
+                return ((DataModel.smeny)smeny).id;
+            }
+            else
+            {
+                DataModel.smeny newSmena = new DataModel.smeny()
+                {
+                    kodUP = kod,
+                    name = name
+                };
+                context.smenies.InsertOnSubmit(newSmena);
+                context.SubmitChanges();
+                return newSmena.id;
+            }
+        }
+
+        public static int saveUP(string kod, string name)
+        {
+            dataClassWorkDataContext context = new dataClassWorkDataContext();
+            var subject = context.uradPraces.Where(x => x.kodUP == kod).FirstOrDefault();
+            if (subject != null)
+            {
+                return ((DataModel.uradPrace)subject).id;
+            }
+            else
+            {
+                DataModel.uradPrace newsubject = new DataModel.uradPrace()
+                {
+                    kodUP = kod,
+                    name = name
+                };
+                context.uradPraces.InsertOnSubmit(newsubject);
+                context.SubmitChanges();
+                return newsubject.id;
+            }
+        }
+
+        public static int saveObor(string kod, string name)
+        {
+            dataClassWorkDataContext context = new dataClassWorkDataContext();
+            var subject = context.obors.Where(x => x.kodUp == kod).FirstOrDefault();
+            if (subject != null)
+            {
+                return ((DataModel.obor)subject).id;
+            }
+            else
+            {
+                DataModel.obor newsubject = new DataModel.obor()
+                {
+                    kodUp = kod,
+                    name = name
+                };
+                context.obors.InsertOnSubmit(newsubject);
+                context.SubmitChanges();
+                return newsubject.id;
+            }
+        }
+
+        public static int saveProf(string kod, string name, int idObor)
+        {
+            dataClassWorkDataContext context = new dataClassWorkDataContext();
+            var subject = context.profs.Where(x => x.kodUP == kod).FirstOrDefault();
+            if (subject != null)
+            {
+                return ((DataModel.prof)subject).id;
+            }
+            else
+            {
+                DataModel.prof newsubject = new DataModel.prof()
+                {
+                    kodUP = kod,
+                    name = name,
+                    idObor = idObor
+                };
+                context.profs.InsertOnSubmit(newsubject);
+                context.SubmitChanges();
+                return newsubject.id;
+            }
+        }
+
+
     }
 }
--- WorkListCR/exportData/MainWindow.xaml.cs
+++ WorkListCR/exportData/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace exportData
 {
@@ -23,6 +27,72 @@ namespace exportData
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void buttonExport_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath = "";
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                filePath = ofd.FileName;
+                if(validateFileName(filePath))
+                {
+                    exportXml(filePath);
+                }
+            }
+        }
+
+        private bool validateFileName(string filePath)
+        {
+            return filePath.Contains(".xml");
+        }
+
+        private void exportXml(string filePath)
+        {
+            XmlDocument xd = new XmlDocument();
+            FileStream fs = new FileStream(filePath, FileMode.Open);
+            xd.Load(fs);
+            foreach (XmlElement vacancy in xd.GetElementsByTagName("VOLNEMISTO"))
+            {
+                int idVzdelani = 0;
+                var vzdelani = vacancy.GetElementsByTagName("MIN_VZDELANI");
+                if (vzdelani.Count > 0)
+                {
+                    idVzdelani = DataModel.VolneMistaBLL.saveVzdelani(vzdelani[0].Attributes["kod"].Value, vzdelani[0].Attributes["nazev"].Value);
+                }
+
+                int idSmennost = 0;
+                var smeny = vacancy.GetElementsByTagName("SMENNOST");
+                if (smeny.Count > 0)
+                {
+                    idSmennost = DataModel.VolneMistaBLL.saveSmeny(smeny[0].Attributes["kod"].Value, smeny[0].Attributes["nazev"].Value);
+                }
+
+                int idUP = 0;
+                var UP = vacancy.GetElementsByTagName("URAD_PRACE");
+                if (UP.Count > 0)
+                {
+                    idUP = DataModel.VolneMistaBLL.saveUP(UP[0].Attributes["kod"].Value, UP[0].Attributes["nazev"].Value);
+                }
+
+                int idObor = 0;
+                var obor = vacancy.GetElementsByTagName("OBOR");
+                if (obor.Count > 0)
+                {
+                    idObor = DataModel.VolneMistaBLL.saveObor(obor[0].Attributes["kod"].Value, obor[0].Attributes["nazev"].Value);
+                }
+
+                int idProf = 0;
+                var prof = vacancy.GetElementsByTagName("PROFESE");
+                if (prof.Count > 0)
+                {
+                    idProf = DataModel.VolneMistaBLL.saveObor(prof[0].Attributes["kod"].Value, prof[0].Attributes["nazev"].Value);
+                }
+
+
+
+            }
         }
     }
 }
